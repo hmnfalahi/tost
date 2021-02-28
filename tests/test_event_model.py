3219d184e66688593cb50a4312f5ba4ddb395e3a
Hmn Falahi <hmnfalahi@gmail.com>
2021-03-01 01:00:07 +0330

    coding style

--- tests/test_event_model.py
+++ tests/test_event_model.py
@@ -17,26 +17,30 @@ def test_event(db):
         title='channel1',
     )
     session.add(channel)
+    session.commit()
 
     bot_event = Event(
-        title='event1',
-        description='event1',
-        bot=bot
+        title='bot_event',
+        description='bot_event',
+        bot=bot,
     )
     session.add(bot_event)
 
     channel_event = Event(
-        title='event1',
-        description='event1',
+        title='channel_event',
+        description='channel_event',
     )
     session.add(channel_event)
+    session.commit()
 
     channel.event = channel_event
 
+    assert bot_event.id is not None
     assert bot_event.owner == bot
     assert bot_event.id == bot.event.id
     assert bot_event == bot.event
-
+    assert channel_event.id is not None
     assert channel_event.owner == channel
     assert channel_event.id == channel.event.id
+    assert channel.event == channel_event
 
